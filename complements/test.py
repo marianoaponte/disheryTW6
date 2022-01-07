3a04d611a50558c776416f97ecaa205892d10179
Chiara Criscuolo <chiara.criscuolo001@studenti.uniparthenope.it>
2022-01-07 12:04:11 +0100

    --------

---
+++ complements/test.py
@@ -0,0 +1,18 @@
+def recipe_dictionary(dict):
+    dict = {}
+
+    _id=
+    Name=
+    Portions=
+    Ingredients=
+    Quantity=
+    Measure=
+
+
+
+    for variable in ["name", "portions", "ingredients", "quantity", "measure"]:
+         dict[variable] = eval(variable)
+
+    print(dict)
+    OUTPUT
+    return dict
